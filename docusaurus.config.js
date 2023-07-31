16fe608c2e3d3e692db3445021ac39bf3c174841
WangLei <wangl_msg@163.com>
2023-07-31 23:01:45 +0800

    update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   projectName: "fine-admin-docs", // Usually your repo name.
   trailingSlash: false,
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
